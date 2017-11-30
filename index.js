06b6bdc2324b83f2cb2fae8a5acbefa325119408
Wouter de Vos <wouter@codaisseur.com>
2017-11-30 16:11:57 +0100

    Add Recipe model and recipes index

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+
+// index.js
 const express = require('express')
+const { Recipe } = require('./models') // this works because of the index file!
 
 const PORT = process.env.PORT || 3030
 
 let app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!\n')
+app.get('/recipes', (req, res, next) => {
+  Recipe.find()
+    // Newest recipes first
+    .sort({ createdAt: -1 })
+    // Send the data in JSON format
+    .then((recipes) => res.json(recipes))
+    // Forward any errors to error handler
+    .catch((error) => next(error))
 })
 
 app.listen(PORT, () => {
