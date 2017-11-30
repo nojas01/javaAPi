8b43f1052f1da2f37288cee3fdd488b0a06594c3
Wouter de Vos <wouter@codaisseur.com>
2017-11-30 15:43:30 +0100

    Add newline to res.body

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3030
 let app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+  res.send('Hello from Express!\n')
 })
 
 app.listen(PORT, () => {
