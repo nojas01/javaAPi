0309747557b19a16e1291ce9c2135a9923c7945c
Wouter de Vos <wouter@codaisseur.com>
2017-12-04 16:32:05 +0100

    Seed script

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,7 @@
 // routes/recipes.js
 const router = require('express').Router()
 const { Recipe } = require('../models')
+const passport = require('../config/auth')
 
 router.get('/recipes', (req, res, next) => {
   Recipe.find()
@@ -20,11 +21,30 @@ router.get('/recipes', (req, res, next) => {
       })
       .catch((error) => next(error))
   })
-  .post('/recipes', (req, res, next) => {
-    let newRecipe = req.body
+  .post('/recipes',
+    passport.authorize('jwt', { session: false }),
+    (req, res, next) => {
+      let newRecipe = req.body
+      newRecipe.authorId = req.account._id
 
-    Recipe.create(newRecipe)
-      .then((recipe) => res.json(recipe))
+      Recipe.create(newRecipe)
+        .then((recipe) => res.json(recipe))
+        .catch((error) => next(error))
+    })
+  .put('/recipes/:id', (req, res, next) => {
+    const id = req.params.id
+    Recipe.findById(id)
+      .then((recipe) => {
+        if (!recipe) { return next() }
+
+        const newData = req.body
+
+        recipe.update(newData)
+          .then((updatedRecipe) => {
+            res.json(updatedRecipe)
+          })
+          .catch((error) => next(error))
+      })
       .catch((error) => next(error))
   })
 
--- routes/sessions.js
+++ routes/sessions.js
@@ -5,10 +5,12 @@ const passport = require('../config/auth')
 const { User } = require('../models')
 const jwtOptions = require('../config/jwt')
 
-router.post('/sessions', passport.authenticate('local'), (req, res) => {
-  const payload = { id: req.user.id }
-  const token = jwt.sign(payload, jwtOptions.secretOrKey)
-  res.json({ token })
-})
+router.post('/sessions',
+  passport.authenticate('local'),
+  (req, res) => {
+    const payload = { id: req.user.id }
+    const token = jwt.sign(payload, jwtOptions.secretOrKey)
+    res.json({ token })
+  })
 
 module.exports = router
